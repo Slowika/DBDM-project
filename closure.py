8f2828b7d96cf8d123d0629a4137b84150e81980
Agnieszka Slowik <agnieszka.slowik44@gmail.com>
2017-03-26 14:06:42 +0200

    Closure and closure-improved

--- closure.py
+++ closure.py
@@ -23,32 +23,46 @@ def fds_to_dict(fds):
                 else:
                     values = line[1].split(' ')
 
-                keys = (tuple(filter(lambda x: x.isalnum()==True, keys)))
-                values = (set(filter(lambda x: x.isalnum()==True, values)))
+                keys = (frozenset(filter(lambda x: x.isalnum()==True, keys)))
+                values = (frozenset(filter(lambda x: x.isalnum()==True, values)))
                 fds_dict[keys] = values
     return fds_dict
 
 """
 Main functions
 """
-"""
+
 def Closure(fds, atts):
     Cl = atts
     done = False
     while (not done):
         done = True
-        for key in fds:
-            #key is a tuple and to use issubset you need two sets!!!!
-            keys = set()
-            for a in range (len(key)): #how to "parcourir" all elt in a tuple??
-                keys.add(a)
-            #keys is a set and key is a tuple with the same element. we can use keys now to know if it's a subset of Cl and Z
-            if (keys.issubset(Cl.intersection(fds[key])) and not ((Cl.intersection(fds[key])).issubset(Cl))):
-                Cl = Cl.union(fds[key])
+        for key, value in fds.items():
+            if key.issubset(Cl) and not value.issubset(Cl):
+                Cl = Cl.union(value)
                 done = False
     return Cl
-"""
 
+def Closure_improved(fds, atts):
+    count = {}
+    lista = {}
+    for w, z in fds.items():
+        count[(w,z)] = len(w)
+        for A in w:
+            if len(lista) == 0 or A not in lista:
+                lista[A] = []
+            lista[A].append(tuple([w,z]))
+    closure = atts
+    update = atts
+    while len(update) != 0:
+        for A in update:
+            update = update.difference(A)
+            for (w, z) in lista[A]:
+                count[(w,z)] = count[(w,z)] - 1
+                if count[(w,z)] == 0:
+                    update = update.union(z.difference(closure))
+                    closure = closure.union(z)
+    return closure
 
 """
 Main program
@@ -57,27 +71,34 @@ Main program
 if __name__ == "__main__":
     option = sys.argv[1][1:]
     print ("The chosen option: " + option)
+
     if option == 'generate':
         n = sys.argv[2]
         print ("Generate a particular set of FDs for the integer: " + n)
     else:
         input = sys.argv[2]
 
-    if input == "~":
-        #TODO
-        fds_list = []
-        for fd in sys.argv[3:]:
-            fds_list.append(fd)
+        if input == "~":
+            #TODO
+            fds_list = []
+            for fd in sys.argv[3:]:
+                fds_list.append(fd)
 
-    elif input[-4:] == ".txt":
-        print ("Reading FDs from the file: " + input)
-        file = open(EXAMPLES_DIR + "/" +input, "r")
-        print (fds_to_dict(file))
+        elif input[-4:] == ".txt":
+            print ("Reading FDs from the file: " + input)
+            file = open(EXAMPLES_DIR + "/" +input, "r")
+            fds = fds_to_dict(file)
+            print (fds)
 
-    if len(sys.argv) > 3:
-        atts = set()
-        for att in sys.argv[3:]:
-            if att.isalnum():
-                atts.add(att)
-        print ("Set of attributes: " + str(atts))
+        if len(sys.argv) > 3:
+            atts = set()
+            for att in sys.argv[3:]:
+                if att.isalnum():
+                    atts.add(att)
+            print ("Set of attributes: " + str(atts))
+
+        if option == "naive":
+            print ("Naive closure: " + str(Closure(fds, atts)))
+        elif option == "improved":
+            print ("Improved closure: " + str(Closure_improved(fds, atts)))
 
